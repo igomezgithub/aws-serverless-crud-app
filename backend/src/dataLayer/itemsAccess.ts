e9363c12ea3b100e9630cbef2917434dd517f87f
Iván <ivangomrod@gmail.com>
2021-10-02 14:24:25 +0100

    Add the AWS X-Ray tracing

--- backend/src/dataLayer/itemsAccess.ts
+++ backend/src/dataLayer/itemsAccess.ts
@@ -1,12 +1,13 @@
 import * as AWS  from 'aws-sdk'
-// import * as AWSXRay from 'aws-xray-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
+const XAWS = AWSXRay.captureAWS(AWS)
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 import { UpdateTodoRequest } from '../requests/UpdateTodoRequest';
 import { TodoItem } from '../models/TodoItem'
 import { createLogger } from '../utils/logger';
 
-// const XAWS = AWSXRay.captureAWS(AWS)
-const s3 = new AWS.S3({
+
+const s3 = new XAWS.S3({
   signatureVersion: 'v4'
 })
 
@@ -157,11 +158,11 @@ export class ItemAccess {
 function createDynamoDBClient() {
   if (process.env.IS_OFFLINE) {
     console.log('Creating a local DynamoDB instance')
-    return new AWS.DynamoDB.DocumentClient({
+    return new XAWS.DynamoDB.DocumentClient({
       region: 'localhost',
       endpoint: 'http://localhost:8000'
     })
   }
 
-  return new AWS.DynamoDB.DocumentClient()
+  return new XAWS.DynamoDB.DocumentClient()
 }
