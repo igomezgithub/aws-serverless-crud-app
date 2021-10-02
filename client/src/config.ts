928baf5a723c9ee37e35d14bf6c40c81fe63a8fa
Iván <ivangomrod@gmail.com>
2021-10-02 12:10:22 +0100

    Add a partition key

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'xh91k9wsdi'
+const apiId = 't76y77i0m4'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
 export const authConfig = {
